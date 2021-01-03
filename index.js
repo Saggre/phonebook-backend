f837b0dd5ad6ee17b30f4d46f52b5f69b9296c24
Saggre <sakri.koskimies@hotmail.com>
2021-01-03 22:58:08 +0200

    Add phonebook api functionality (ex 3.1-3.6)

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ let persons = [
     }
 ];
 
+/**
+ * Generate a random id
+ * @returns Number
+ */
+const generateId = () => Math.floor(Math.random() * (~0 >>> 1)) + 1;
+
 app.get('/info', (req, res) => {
     res.write(`Phonebook has info for ${persons.length} people\n\n`);
     res.write(new Date().toString());
@@ -34,6 +40,48 @@ app.get('/info', (req, res) => {
 
 app.get('/api/persons', (req, res) => {
     res.json(persons);
+});
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'content missing'
+        });
+    }
+
+    if (persons.filter(person => person.name === body.name).length > 0) {
+        return res.status(409).json({
+            error: 'name must be unique'
+        });
+    }
+
+    const person = {
+        'name': body.name,
+        'number': body.number,
+        'id': generateId()
+    };
+
+    persons = [...persons, person];
+
+    res.json(person);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).end();
+    }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter(note => note.id !== id);
+    res.status(204).end();
 });
 
 
