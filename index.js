6fae7e0fd1856e6e8d0a68637c190e8781174eec
Saggre <sakri.koskimies@hotmail.com>
2021-01-05 21:51:58 +0200

    Modify for deployment

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 morgan.token('body', function (req, res) {
     return JSON.stringify(req.body);
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
@@ -94,7 +96,7 @@ app.use((req, res) => {
     res.status(404).send({error: 'unknown endpoint'});
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
