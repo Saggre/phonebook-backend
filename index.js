fc70ecd58ef6a6e0f980cb7bac472343c95d118d
Saggre <sakri.koskimies@hotmail.com>
2021-01-05 22:13:53 +0200

    Modify for deployment

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ morgan.token('body', function (req, res) {
     return JSON.stringify(req.body);
 });
 
+app.use(express.static('build'));
 app.use(cors());
 app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {}));
 
 let persons = [
     {
