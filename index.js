44d8c957bed43c77691a8f313ae3b92143bd7c8a
Saggre <sakri.koskimies@hotmail.com>
2021-01-06 22:27:14 +0200

    Add db functionality (ex. 3.13-3.16)

--- index.js
+++ index.js
@@ -1,101 +1,83 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
+const errorHandler = require('./middleware/errorHandler');
+const Person = require('./models/person');
 
 morgan.token('body', function (req, res) {
     return JSON.stringify(req.body);
 });
 
 app.use(express.static('build'));
-app.use(cors());
 app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {}));
+app.use(cors());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-    {
-        'name': 'Arto Hellas',
-        'number': '040-123456',
-        'id': 1
-    },
-    {
-        'name': 'Ada Lovelace',
-        'number': '39-44-5323523',
-        'id': 2
-    },
-    {
-        'name': 'Dan Abramov',
-        'number': '12-43-234345',
-        'id': 3
-    },
-    {
-        'name': 'Mary Poppendieck',
-        'number': '39-23-6423122',
-        'id': 4
-    }
-];
-
-/**
- * Generate a random id
- * @returns Number
- */
-const generateId = () => Math.floor(Math.random() * (~0 >>> 1)) + 1;
-
-app.get('/info', (req, res) => {
-    res.write(`Phonebook has info for ${persons.length} people\n\n`);
-    res.write(new Date().toString());
-    res.end();
+app.get('/api/persons', (req, res, next) => {
+    Person.find({}).then(persons => {
+        res.json(persons.map(person => person.toJSON()));
+    }).catch(error => next(error));
 });
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons);
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id).then(person => {
+        if (person) {
+            res.json(person.toJSON());
+        } else {
+            res.status(404).end();
+        }
+    }).catch(error => next(error));
 });
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body;
 
-    if (!body.name || !body.number) {
+    if (!'name' in body || !'number' in body) {
         return res.status(400).json({
             error: 'content missing'
         });
     }
 
-    if (persons.filter(person => person.name === body.name).length > 0) {
-        return res.status(409).json({
-            error: 'name must be unique'
-        });
-    }
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    });
+
+    person.save().then(person => {
+        res.json(person.toJSON());
+    }).catch(error => next(error));
+});
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body;
 
     const person = {
-        'name': body.name,
-        'number': body.number,
-        'id': generateId()
+        name: body.name,
+        number: body.number
     };
 
-    persons = [...persons, person];
-
-    res.json(person);
+    Person.findByIdAndUpdate(req.params.id, person, {new: true})
+        .then(person => {
+            res.json(person);
+        })
+        .catch(error => next(error));
 });
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const person = persons.find(person => person.id === id);
-    if (person) {
-        res.json(person);
-    } else {
-        res.status(404).end();
-    }
-});
-
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    persons = persons.filter(note => note.id !== id);
-    res.status(204).end();
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end();
+        })
+        .catch(error => next(error));
 });
 
 app.use((req, res) => {
     res.status(404).send({error: 'unknown endpoint'});
 });
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
